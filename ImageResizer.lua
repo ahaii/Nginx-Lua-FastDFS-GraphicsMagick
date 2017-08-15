0b731c653c7450cfa0b5ee679b56ed5159049b1f
ahaii <yuliu100@sina.com>
2017-08-15 19:10:16 +0800

    Update ImageResizer.lua

--- ImageResizer.lua
+++ ImageResizer.lua
@@ -1,3 +1,3 @@
-local command = "/shenghuo/local/GraphicsMagick/bin/gm convert " .. ngx.var.request_filepath .. " -resize " .. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. "." .. ngx.var.ext;
+local command = "/usr/local/GraphicsMagick/bin/gm convert " .. ngx.var.request_filepath .. " -resize " .. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. "." .. ngx.var.ext;
 os.execute(command);
 ngx.exec(ngx.var.request_uri);
