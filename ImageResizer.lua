aceb7669fb9b2c2b9d36a25082727c37f762a9b1
ahaii <yuliu100@sina.com>
2017-12-29 18:27:23 +0800

    解决请求大尺寸图片时占用较大IO问题

--- ImageResizer.lua
+++ ImageResizer.lua
@@ -1,3 +1,15 @@
-local command = "/usr/local/GraphicsMagick/bin/gm convert " .. ngx.var.request_filepath .. " -resize " .. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. "." .. ngx.var.ext;
-os.execute(command);
-ngx.exec(ngx.var.request_uri);
+if(ngx.var.height ~= '' and ngx.var.width ~= '') then
+    height = tonumber(ngx.var.height);
+    width = tonumber(ngx.var.width);
+    if(height > 1000) then
+        height = 1000
+    end
+    if(width > 1000) then
+        height = 1000
+    end
+    local command = "/data/local/GraphicsMagick/bin/gm convert " .. ngx.var.request_filepath .. " -resize " .. width .. "x" .. height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. "." .. ngx.var.ext;
+    os.execute(command);
+    ngx.exec(ngx.var.request_uri);
+else
+    ngx.exit(ngx.HTTP_NOT_FOUND);
+end
